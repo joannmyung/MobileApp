0d6a466171804c2f73aedb3404412a569b4141a9
joannmyung <123985374+joannmyung@users.noreply.github.com>
2023-02-21 12:51:56 -0500

    Add files via upload

---
+++ week4_sound/Page1.swift
@@ -0,0 +1,20 @@
+//
+//  Page1.swift
+//  week4_sound
+//
+//  Created by Joann Myung on 2/21/23.
+//
+
+import SwiftUI
+
+struct Page1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page1_Previews: PreviewProvider {
+    static var previews: some View {
+        Page1()
+    }
+}
